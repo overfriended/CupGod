9e9646be980a302827c821707ab14ea2a1913ccc
VRPCaden <giantballofcrap@gmail.com>
2019-02-12 19:35:03 -0400

    Add project files.

---
+++ CupGod/CupGod.cs
@@ -0,0 +1,40 @@
+﻿using Smod2;
+using Smod2.API;
+using Smod2.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CupGod
+{
+    [PluginDetails(
+        author = "overfriended",
+        name = "Test",
+        description = "CupGod is a plugin that when you drop a cup, you become god. You cannot do damage.",
+        id = "overfriended.cup.god",
+        version = "1.0",
+        SmodMajor = 3,
+        SmodMinor = 0,
+        SmodRevision = 0
+        )]
+    public class CupGod : Plugin
+    {
+
+        public override void OnDisable()
+        {
+            this.Info(this.Details.name + "disabled");
+        }
+
+        public override void OnEnable()
+        {
+            this.Info(this.Details.name + "enabled");
+        }
+
+        public override void Register()
+        {
+            this.AddEventHandlers(new EventHandler(this));
+        }
+    }
+}
---
+++ CupGod/EventHandler.cs
@@ -0,0 +1,56 @@
+﻿using Smod2.API;
+using Smod2.EventHandlers;
+using Smod2.Events;
+using AdminToolbox.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CupGod
+{
+    class EventHandler : IEventHandlerPlayerDropItem, IEventHandlerPlayerHurt
+    {
+        private readonly CupGod plugin;
+
+        List<string> playerSteamId = new List<string>();
+
+
+        public EventHandler(CupGod plugin)
+        {
+            this.plugin = plugin;
+        }
+
+        public String getSteamIdFromName(String name)
+        {
+            Player player = GetPlayerFromString.GetPlayer(name);
+            return player.SteamId;
+        }
+
+        public void OnPlayerDropItem(PlayerDropItemEvent ev)
+        {
+            if (ev.Item.ItemType == Smod2.API.ItemType.CUP)
+            {
+                PlayerSettings playerSettings = new PlayerSettings(ev.Player.SteamId);
+                playerSettings.godMode = true;
+
+                playerSteamId.Add(ev.Player.SteamId);
+            }
+        }
+
+        public void OnPlayerHurt(PlayerHurtEvent ev)
+        {
+            if (playerSteamId.Contains(ev.Attacker.SteamId))
+            {
+                ev.Damage = 0;
+            }
+        }
+
+        public void OnRoundEnd(IEventHandlerRoundEnd ev)
+        {
+            playerSteamId.Clear();
+        }
+
+    }
+}
