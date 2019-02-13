a9e821c766fdf878a6a028fdb4ae81b7a6848554
VRPCaden <giantballofcrap@gmail.com>
2019-02-13 17:24:22 -0400

    1.0.2

--- CupGod/CupGod.cs
+++ CupGod/CupGod.cs
@@ -14,14 +14,46 @@ namespace CupGod
         name = "Cup God",
         description = "CupGod is a plugin that when you drop a cup, you become god. You cannot do damage.",
         id = "overfriended.cup.god",
-        version = "1.0.1",
+        version = "1.0.2",
         SmodMajor = 3,
         SmodMinor = 0,
         SmodRevision = 0
         )]
     public class CupGod : Plugin
     {
+        public static CupGod plugin;
 
+        public static int CupGods = 0;
+
+        public static int CountRoles(Role role)
+        {
+            int count = 0;
+            foreach (Player pl in PluginManager.Manager.Server.GetPlayers())
+                if (pl.TeamRole.Role == role)
+                    count++;
+            return count;
+        }
+
+        public static int CountRoles(Smod2.API.Team team)
+        {
+            int count = 0;
+            foreach (Player pl in PluginManager.Manager.Server.GetPlayers())
+                if (pl.TeamRole.Team == team)
+                    count++;
+            return count;
+        }
+
+        public class PlayerInfo
+        {
+            public PlayerInfo(string badgename, string badgecolor)
+            {
+                BadgeColor = badgecolor;
+                BadgeName = badgename;
+            }
+
+            public string BadgeName;
+            public string BadgeColor;
+        }
         public override void OnDisable()
         {
             this.Info(this.Details.name + " disabled");
--- CupGod/EventHandler.cs
+++ CupGod/EventHandler.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace CupGod
 {
-    class EventHandler : IEventHandlerPlayerDropItem, IEventHandlerPlayerHurt
+    class EventHandler : IEventHandlerPlayerDropItem, IEventHandlerPlayerHurt, IEventHandlerPlayerDie
     {
         private readonly CupGod plugin;
 
@@ -32,10 +32,12 @@ namespace CupGod
         {
             if (ev.Item.ItemType == Smod2.API.ItemType.CUP)
             {
-                ev.Player.SetGodmode(true);
                 playerSteamId.Add(ev.Player.SteamId);
 
-                ev.Player.SendConsoleMessage("You're god!");
+                ev.Player.SendConsoleMessage("The Cup of God has granted your wishes, and you now have the blessings of God!");
+                ev.Player.SetRank("blue_green", "Blessed");
+                CupGod.CupGods++;
+
             }
         }
 
@@ -45,12 +47,38 @@ namespace CupGod
             {
                 ev.Damage = 0;
             }
+            if (playerSteamId.Contains(ev.Player.SteamId))
+            {
+                ev.Damage = 0;
+                ev.Attacker.Damage(10, DamageType.NUKE);
+            }
         }
 
         public void OnRoundEnd(IEventHandlerRoundEnd ev)
         {
             playerSteamId.Clear();
         }
+        public void OnCheckRoundEnd(CheckRoundEndEvent ev)
+        {
 
+            bool MTFAlive = CupGod.CountRoles(Smod2.API.Team.NINETAILFOX) > 0;
+            bool CiAlive = CupGod.CountRoles(Smod2.API.Team.CHAOS_INSURGENCY) > 0;
+            bool ScpAlive = CupGod.CountRoles(Smod2.API.Team.SCP) > 0;
+            bool DClassAlive = CupGod.CountRoles(Smod2.API.Team.CLASSD) > 0;
+            bool ScientistsAlive = CupGod.CountRoles(Smod2.API.Team.SCIENTIST) > 0;
+            int CupGodCount = CupGod.CupGods;
+            bool CGAlive = CupGodCount > 0;
+
+            if (MTFAlive && (CiAlive || ScpAlive || DClassAlive || CGAlive))
+                ev.Status = ROUND_END_STATUS.ON_GOING;
+            else if (CiAlive && (MTFAlive || (DClassAlive && ScpAlive) || ScientistsAlive || CGAlive))
+                ev.Status = ROUND_END_STATUS.ON_GOING;
+            else if (ScpAlive && (MTFAlive || DClassAlive || ScientistsAlive))
+                ev.Status = ROUND_END_STATUS.ON_GOING;
+            else if (CGAlive && ScpAlive && !MTFAlive && !CiAlive && !DClassAlive && !ScientistsAlive)
+                ev.Status = ROUND_END_STATUS.SCP_VICTORY;
+            else if (CiAlive && ScpAlive)
+                ev.Status = ROUND_END_STATUS.SCP_CI_VICTORY;
+        }
     }
 }
